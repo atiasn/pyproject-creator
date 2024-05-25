51da0c2cc3272b36235851d49befa8b753418ea3
atiasn <riboylei@gmail.com>
2024-05-25 22:54:40 +0800

    ✨ feat: add github actio

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
-import shutil
 import sys
+import shutil
 from pathlib import Path
 
 import pytest
@@ -7,20 +7,21 @@ import pytest
 from pyproject_creator.cli import BASE_PATH
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def rootdir(request):
     return Path(request.config.rootdir)
 
 
-@pytest.fixture(scope='session', autouse=True)
+@pytest.fixture(scope="session", autouse=True)
 def add_python_path():
     sys.path.insert(0, str(BASE_PATH.parent.resolve()))
 
 
-@pytest.fixture(scope="function")
-def project_name(rootdir):
-    _name = "a-test-create-project"
-    _path = rootdir / _name
-    if _path.exists():
-        shutil.rmtree(_path)
-    yield _name
+@pytest.fixture(scope="session")
+def project_names(rootdir):
+    _names = ["a-test-create-project", "a-test-create-project-without"]
+    for _name in _names:
+        _path = rootdir / _name
+        if _path.exists():
+            shutil.rmtree(_path.resolve())
+    return _names
